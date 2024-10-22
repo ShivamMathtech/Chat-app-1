67dc68ad377d8735c6d85b87850d5cb2552f8bff
ShivamMathtech <drshivam49@gmail.com>
2024-10-22 19:32:28 +0530

    vsersion 1.0.0

--- backend/controllers/loginctrls.js
+++ backend/controllers/loginctrls.js
@@ -1,18 +1,20 @@
 const User = require("../model/user");
 
 let loginCtrls = (req, res) => {
-  // Login logic here
-  User.findOne({ email: req.body.email, password: req.body.password }).then(
+  User.find({ fname: req.body.name, password: req.body.password }).then(
     (user) => {
-      if (!user)
-        return res.status(401).json({ message: "Invalid credentials" });
-      req.session.userId = user._id;
-      res.json({ message: "Login successful", data: user });
+      if (user === null) {
+        res.status(403).json({
+          message: "User not found",
+        });
+      } else {
+        res.status(200).json({
+          message: "Login successful",
+          data: user,
+        });
+      }
     }
   );
-  res.status(200).json({
-    message: "Login controls route",
-  });
 };
 
 module.exports = loginCtrls;
